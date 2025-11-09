d2854acae154f95f3289875f4d8ab2e7779a199a
Peter Chang <chang.p.l@wustl.edu>
2025-11-08 21:09:03 -0600

    Added useState to make square display 'X' when clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { useState } from 'react';
+
 // Square component
-function Square({ value }) {
+function Square() {
+  // React function to "remember" state
+  const [value, setValue] = useState(null);
+
   function handleClick() {
     console.log('clicked!');
+    setValue('X'); // Display 'X' when square is clicked
   }
   
   return (
@@ -19,19 +25,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
