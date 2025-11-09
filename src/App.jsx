4d6f73b1b14eaf50161b2a73f4367ef967c36cb7
Peter Chang <chang.p.l@wustl.edu>
2025-11-08 21:37:34 -0600

    Created game component to store game history

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function Square({ value, onSquareClick }) {
   // );
 }
 
-export default function Board() {
+function Board() {
   // Keeps track of whose turn it is
   const [xIsNext, setXIsNext] = useState(true);
 
@@ -87,6 +87,19 @@ export default function Board() {
     </>
   );
 } 
+
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
+}
 
 // Check for a winner
 function calculateWinner(squares) {
