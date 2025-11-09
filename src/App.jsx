a65d655983edaa5f7692f30cf657458c318bcef4
Peter Chang <chang.p.l@wustl.edu>
2025-11-08 21:29:06 -0600

    Created function to check for a winner

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ export default function Board() {
   // Creating a copy of data allows for "time travel" / "undo" features
   // As opposed to directly editing squares array
   function handleClick(i) {
-    
+
     // Exit function if square is already filled
     if (squares[i]) {
       return;
@@ -77,3 +77,24 @@ export default function Board() {
     </>
   );
 } 
+
+// Check for a winner
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
