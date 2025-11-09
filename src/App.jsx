5b889af986690ca5ec611670bdf06d8138bbf84c
Peter Chang <chang.p.l@wustl.edu>
2025-11-08 21:15:56 -0600

    Declared values in parent (board) using array, then passed down to children (squares) via prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,51 @@
 import { useState } from 'react';
 
 // Square component
-function Square() {
-  // React function to "remember" state
-  const [value, setValue] = useState(null);
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+  // // React function to "remember" state
+  // const [value, setValue] = useState(null);
 
-  function handleClick() {
-    console.log('clicked!');
-    setValue('X'); // Display 'X' when square is clicked
-  }
+  // // Display 'X' when square is clicked
+  // function handleClick() {
+  //   console.log('clicked!');
+  //   setValue('X'); 
+  // }
   
-  return (
-    <button 
-      className="square"
-      onClick={handleClick}
-    >
-      {value}
-    </button>
-  );
+  // return (
+  //   <button 
+  //     className="square"
+  //     onClick={handleClick}
+  //   >
+  //     {value}
+  //   </button>
+  // );
 }
 
 // Board with 3 rows, each with 3 squares
 export default function Board() {
+
+  // Array of 9 nulls corresponding to 9 squares to store game state
+  // Declare state in parent (board) instead of children (square)
+  // Pass state down to children with props
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
